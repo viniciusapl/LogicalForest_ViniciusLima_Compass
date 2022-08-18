1f5c7af60c4a306282edfc9e23a0fccc286e2e17
viniciusapl <vinialexa1@gmail.com>
2022-08-18 20:45:00 -0300

    anotações js e início do koans

---
+++ javascript/script.js
@@ -0,0 +1,83 @@
+//Aula de Variáveis
+
+// let idade = 24;
+// console.log(idade);
+
+//nomeação de variáveis é realizada em camelCase, onde a primeira letra é minúscula e a segunda palavra inicia com a letra maiúscula.
+
+
+//Aula de constantes
+
+// const valorIngressoAdulto = 20;
+
+// console.log (valorIngressoAdulto);
+
+// Utilizar o const quando a variável for constante para que não seja permitida a troca de valores dessa variável.
+
+
+//Tipos Primitivos
+
+//Tipos de referência => String, Number, Boolean, Undefined, null
+
+//String: vários caracteres em uma palavra.
+//Number: número literal.
+//Boolean: true or false
+//Undefined: valor padrão de variável
+//Null: serve para ser resetado. (valor nulo)
+
+//Objetos
+
+//A função de um objeto é juntar informações em um lugar só
+
+//Para criar um objeto em JS deve ser feito da seguinte forma:
+
+//let pessoa = {
+//    nome: Rafael
+    // Sobrenome: Ribeiro
+    // Idade: 35
+    // Altura: null
+    // Gênero: 'Masculino'
+//};
+
+//console.log(pessoa);
+
+//Arrays
+
+//"Um conjunto de objetos"
+
+//Funções
+
+//para nomear uma função 
+
+//verbo + substantivo
+//ex.: function resetaCor(){
+//   corSite = ""; 
+// };
+
+//console.log(corSite);
+//resetaCor();
+//console.log(corSite);
+
+//Operadores
+
+//Aritméticos
+
+// + - * / **
+
+//Atribuição
+
+// = == ===
+//Comparação
+
+//= >= <= !=
+//Lógicos
+
+//and && 
+//or ||
+
+
+//Bitwise
+
+
+
+//
